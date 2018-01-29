ce4da5dcfd9459f98a3fcf9ccf8b340da21d822e
Carl Preusser <crp.geo.logic@gmail.com>
2018-01-28 22:56:13 -0500

    syncing initial

---
+++ NewSepticGeoCoder/NewSeptic.py
@@ -0,0 +1,97 @@
+import arcpy
+import os
+
+class Toolbox(object):
+    def __init__(self):
+        self.label =  "NewSeptic"
+        self.alias  = "NewSeptic"
+
+        # List of tool classes associated with this toolbox
+        self.tools = [GetViableContours]
+
+class GetViableContours(object):
+    def __init__(self):
+        self.label       = "Get Contour Lines"
+        self.description = "Get all contours within a polygon parcel."
+
+    def getParameterInfo(self):
+        #Define parameter definitions
+
+        # Contour Features parameter
+        contour_lines = arcpy.Parameter(
+            displayName="Contour Features",
+            name="contour_lines",
+            datatype="GPFeatureLayer",
+            parameterType="Required",
+            direction="Input")
+
+        contour_lines.filter.list = ["Polyline"]
+
+        # Contour Features parameter
+        parcel_polygon = arcpy.Parameter(
+            displayName="Parcel Features",
+            name="parcel_polygon",
+            datatype="GPFeatureLayer",
+            parameterType="Required",
+            direction="Input")
+
+        parcel_polygon.filter.list = ["Polygon"]
+
+        # Parcel Number Features parameter
+        parcel_number = arcpy.Parameter(
+            displayName="Parcel Number",
+            name="parcel_number",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input")
+
+        # Derived Output Features parameter
+        out_features = arcpy.Parameter(
+            displayName="Contour Features",
+            name="out_features",
+            datatype="GPFeatureLayer",
+            parameterType="Derived",
+            direction="Output")
+
+        out_features.parameterDependencies = [contour_lines.name]
+        out_features.schema.clone = True
+
+        parameters = [contour_lines, parcel_polygon, parcel_number, out_features]
+
+        return parameters
+
+    def isLicensed(self): #This will verify the license is valid
+        #if license is not valid
+        return True
+
+    #def updateParameters(self, parameters): #optional
+    #    if parameters[0].altered:
+    #        parameters[1].value = arcpy.ValidateFieldName(parameters[1].value,
+    #                                                      parameters[0].value)
+    #    return
+
+    def updateMessages(self, parameters): #optional
+        return
+
+    def execute(self, parameters, messages):
+    
+        arcpy.env.overwriteOutput = True
+        contourLines = parameters[0].valueAsText
+        parcelPolygon = parameters[1].valueAsText
+        parcelId = parameters[2].valueAsText
+        
+        mxd = arcpy.mapping.MapDocument(r"C:\Users\crpge\Desktop\Medina\Medina.mxd")
+        df = arcpy.mapping.ListDataFrames(mxd, "New Data Frame")
+        
+        #input = parcelPolygon
+        #in_path = arcpy.Describe(input).catalogPath
+        #out_path = os.path.join(os.path.dirname(in_path), "output")
+		output_Layer = "ourFoundParcel"
+		
+        arcpy.MakeFeatureLayer_management(parcelPolygon, "parcelOutput")
+        #arcpy.SelectLayerByAttribute_management ('parcelOutput', 'NEW_SELECTION', '"PPNUMBER" = "03608A08027"')
+        #arcpy.SelectLayerByAttribute_management ('lyr', 'NEW_SELECTION', '"PPNUMBER" =  {}'.format(parcelId))
+        arcpy.CopyFeatures_management("parcelOutput", outLayer)
+        addLayer = arcpy.mapping.Layer(outLayer)
+        arcpy.mapping.AddLayer(df, addLayer, "BOTTOM")
+        arcpy.RefreshTOC()
--- NewSepticGeoCoder/NewSepticGeoCoder.py
+++ NewSepticGeoCoder/NewSepticGeoCoder.py
@@ -1,1 +1,38 @@
 
+from geopy.geocoders import Nominatim
+import csv
+import os
+import time
+
+geolocator = Nominatim()
+
+csvF = os.path.abspath("C:\InstallerList.csv")
+
+def do_geocode(address):
+    try:
+        return geolocator.geocode(address)
+    except GeocoderTimedOut:
+        return do_geocode(address)
+
+with open(csvF, "rt", encoding="utf8") as csvfile:
+    with open('Output.csv', "a") as csvWriteFile:
+        fReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+         
+        for row in fReader:
+            if (row[5] != "Address") and (row[5] != ""): 
+                companyName = row[0]
+                address = row[5] + " " + row[6] + " " + row[7] + " OH " + row[8]  
+
+                location = do_geocode(address) 
+                time.sleep(0.5)
+                if location is not None:
+                    lat = location.latitude
+                    lon = location.longitude  
+                    
+                    strLat = str(lat)
+                    strLon = str(lon)
+
+                    line = companyName + ", " + address + ", " + strLat + ", " + strLon + "\n"
+
+                    csvWriteFile.write(line)
+                
